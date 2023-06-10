4f3919c9bf0cdd1eb3a0bfdd6f1af7cc92f0741b
Tempo Quill <jicrapo@gmail.com>
2023-06-10 10:41:40 -0400

    Red NSF

--- header.asm
+++ header.asm
@@ -1,6 +1,28 @@
-
+IFNDEF NSF_ROM
 	.db "NES", $1a
 	.db 8  ; 128K PRG
 	.db 16 ; 128K CHR
 	.db MMC1 << 4
 	.db 0, 0, 0, 0, "aster"
+ELSE
+	.db "NESM", $1a ; handshake
+	.db $1 ; version
+	.db NSF_Music_End - NSF_Music
+	.db $1 ; starting song
+	.dw LOAD
+	.dw INIT
+	.dw PLAY
+	.db "Yoshi's Egg/Yoshi/Mario & Yoshi"
+	.db 0
+	.db "Junichi Masuda"
+	.dsb 18, 0
+	.db "'91, '96, Game Freak inc"
+	.dsb 8, 0
+	.dw $411a ; NTSC
+	.db 1, 2, 3, 4, 5, 6, 7, 0
+	.dw 0 ; PAL, unused
+	.db 0 ; this is an NTSC file
+	.db 0 ; no extra chip (North American settings)
+	.dsb 4, 0 ; proceeding data is program data
+
+ENDIF
--- src/config.asm
+++ src/config.asm
@@ -1,5 +1,6 @@
 
 .ignorenl
 ; LOOP_BUG_FIX                = 1
-; POKEMON_RED_MUSIC_SOUNDFONT = 1
+POKEMON_RED_MUSIC_SOUNDFONT = 1
+NSF_ROM = 1
 .endinl
